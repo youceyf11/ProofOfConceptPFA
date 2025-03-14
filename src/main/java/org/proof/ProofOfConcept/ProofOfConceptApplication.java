2854175433bdf665d0f609226098ccab0aa10ffd
youceyf11 <Youssef.Ouriniche@emsi-edu.ma>
2025-03-14 22:19:39 +0000

    Initial commit

---
+++ src/main/java/org/proof/ProofOfConcept/ProofOfConceptApplication.java
@@ -0,0 +1,13 @@
+package org.proof.ProofOfConcept;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProofOfConceptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProofOfConceptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/proof/ProofOfConcept/ProofOfConceptApplicationTests.java
@@ -0,0 +1,13 @@
+package org.proof.ProofOfConcept;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProofOfConceptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
